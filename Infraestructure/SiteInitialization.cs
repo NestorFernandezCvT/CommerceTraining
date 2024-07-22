c3973f0e193288932ce6f80d52d4920fb961b180
Nestor Arturo Fernandez Ricaurte <nestorarturo@hotmail.com>
2024-07-22 13:40:00 -0500

    Add project files.

---
+++ Infraestructure/SiteInitialization.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Commerce.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace CommerceTraining.Infraestructure;
+
+[InitializableModule]
+[ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+public class SiteInitialization : IInitializableModule
+{
+    public void Initialize(InitializationEngine context)
+    {
+        CatalogRouteHelper.MapDefaultHierarchialRouter(false);
+    }
+
+    public void Preload(string[] parameters) { }
+
+    public void Uninitialize(InitializationEngine context)
+    {
+    }
+}
